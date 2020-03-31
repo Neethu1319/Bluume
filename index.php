7f39a7f711aa36721e177f7c8c3e082be832be94
Neethu1319 <62928835+Neethu1319@users.noreply.github.com>
2020-03-31 19:08:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 echo "Welcome to Bluume";
-$conn = mysqli_connect(getenv('HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
-print_r(getenv('HOST'));
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}else{
-  echo "success";
-}
 ?>
